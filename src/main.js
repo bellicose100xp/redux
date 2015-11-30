e925ffe6b232c8b3534b76cc216d683a4463581e
bellicose <bellicose.agnostic@gmail.com>
2015-11-30 09:13:31 -0800

    "working boilerplate"

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <h1>Redux! all the way!</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
