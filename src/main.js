87e71e79a5e0cba407019b66a5db2e8d4f698767
bellicose100xp <chandan7@icloud.com>
2015-11-30 19:50:28 -0800

    added expect library raised javascript level to flow in webstorm IDE did some tests

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import expect from 'expect';
+
+let counter = (state: number, action: Object): number => {
+    switch(action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+expect(counter(0, {type: 'INC'})).toEqual(1);
+expect(counter(1, {type: 'INC'})).toEqual(2);
+expect(counter(2, {type: 'DEC'})).toEqual(1);
+console.log('All Tests Passed');
+
 
 class App extends React.Component {
     render() {
-        return <h1>Redux! all the way!</h1>
+        return <h1>Redux all the way!</h1>
     }
 }
 
